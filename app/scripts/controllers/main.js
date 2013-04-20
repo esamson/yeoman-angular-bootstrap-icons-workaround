e115dc9ffea125cf3f29610b43fecfe18dda6382
Edward Samson <edward@samson.ph>
2013-04-20 18:00:52 +0800

    1. Generate app

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('5425451App')
+  .controller('MainCtrl', ['$scope', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  }]);
